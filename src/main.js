6160df7af33d5e6b4e0f7ef4cecbac19dc655795
LucasHazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-08-29 17:18:39 +0200

    update script

--- src/main.js
+++ src/main.js
@@ -1,18 +1,77 @@
 const { invoke } = window.__TAURI__.tauri;
 
-let greetInputEl;
-let greetMsgEl;
+let equationInput;
+let buttonHolder;
+let lastSign = false;
 
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+async function compute() {
+  equationInput.value = await invoke("compute", { equation: equationInput.value });
+}
+
+const grid = [
+  '1', '2', '3',
+  '4', '5', '6',
+  '7', '8', '9',
+  '0', '.', '+',
+  '-', '*', '/', 
+  '^', '(', ')', 
+  '←', 'C', '='
+];
+
+const specialActions = {
+  'C': () => {
+    lastSign = false;
+    equationInput.value = "";
+  },
+  '=': () => {
+    compute(equationInput.value);
+  },
+  '←': () => {
+    equationInput.value = equationInput.value.substring(0, 
+      equationInput.value.length-2) + " ";
+  }
+};
+
+function updateEquation(input) {
+  if(equationInput.value == 'Invalid') {
+    specialActions['C']();
+  }
+
+  const isCurrentSign = /^[\d\.]$/.test(input);
+
+  if(lastSign && isCurrentSign) {
+    equationInput.value = equationInput.value.substring(0, 
+      equationInput.value.length-1);
+  }
+  
+  equationInput.value += input + " ";
+  
+  lastSign = isCurrentSign;  
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
+  buttonHolder = document.querySelector("#button-holder");
+  equationInput = document.querySelector("#equation-input");
+
+  grid.forEach(element => {
+    const elementNode = document.createElement("button");
+    elementNode.innerText = element;
+
+    if(specialActions[element] != undefined) {
+      elementNode.addEventListener("click", specialActions[element]);
+    } else {
+      elementNode.addEventListener("click", () => {
+        updateEquation(element);
+      });
+    }
+
+    buttonHolder.appendChild(elementNode);
+  });
+
+  document.addEventListener("keydown", (e) => {
+    const action = e.key.toLocaleLowerCase();
+    if(action == 'enter') {
+      specialActions['=']();
+    }
   });
 });
